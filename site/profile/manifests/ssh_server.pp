e494ff7e8eb357cd00ee3fb5a5f11e197145c4d5
acols <a_cols@hotmail.com>
2018-12-20 12:28:11 -0300

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCqwbSin6hpbN3YZHjkNcNl9VFkwE9qmtoJ1cFDGgOWyt/f0H6dPr8yyIzpi0E08GgWRlKAz1rRNY8Ik+nxF38rEuBmJjsksKorD/xp1bQ0QsBmhIzThfZzGR9XBsSIvG8bNzg1pwhZNYzslcGCKSbhGJNtE7fJztOKCqlVSMKLW9p94AsIWE7Ab2iDwls7fOXgLGGoyN8MvXuzStoWE2kJugTYaiCs8AHD6IQX3Odkv4fXgB+jvyAU6XZzB8P5R82222/3KRCqTCSnIR7m5/M01WVxCOov3NLb+D9FWCGZm15GVekOjii3UH8DLQUzIHX2OOGoSfkalFGJNB6qad65',
 	}  
 }
